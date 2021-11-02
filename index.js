d89b9b11de3328c983af2cbb13d3b6fb79b353a4
Morgan <morganlbyrne@gmail.com>
2021-11-02 16:46:10 -0400

    commit tree 75359c54551a18f0a67bae611b39da1d073c692f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+saturdayFun('go to a museum');
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(string = "*") {
+    return function (parameter = "special") {
+        return `You are ${string}${parameter}${string}!`
+    }
+  };
+  
+  wrapAdjective("%")("a dedicated programmer"); 
